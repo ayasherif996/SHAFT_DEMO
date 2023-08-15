4eb0c3956b9692a09ba059015cad7581b91f022d
ayasherif996 <72095765+ayasherif996@users.noreply.github.com>
2023-08-15 17:15:55 +0300

    Change in test case

--- src/test/java/testJava/TestRegisterUser_pom.java
+++ src/test/java/testJava/TestRegisterUser_pom.java
@@ -27,16 +27,16 @@ public class TestRegisterUser_pom {
         new HomePage(driver).AssertOnHomePage("AutomationExercise");
         new MainMenuPage(driver).ClickOnSignUpButton();
         new FirstSignUpPage(driver).AssertOnNewUserSignupStatment("New User Signup!");
-        new FirstSignUpPage(driver).SignUpUser("Aya Sherif", "ayasherif@gmail.com");
+        new FirstSignUpPage(driver).SignUpUser("Aya Sherif", "ayasherif"+System.currentTimeMillis()+"@gmail.com");
         new SecondSignUpPage(driver).AssertOnUrlOnSignUpPage("https://automationexercise.com/signup");
         new SecondSignUpPage(driver).EnterAccountInformation("12345", 3, "March", "2001");
         new SecondSignUpPage(driver).EnterAddressInformation("Aya", "Sherif",
                 "8 El7las street,cairo", "Canada",
                 "Haram11", "cairo", "234521", "0107079982");
         new AccountCreatedPage(driver).AssertOnCreatedAccount("ACCOUNT CREATED!");
-        new AccountCreatedPage(driver).ClickOnContinueButton();
-        new MainMenuPage(driver).ClickOnDeleteButton();
-        new AccountDeletedPage(driver).AssertOnDeletionAnAccount("ACCOUNT DELETED!");
+//        new AccountCreatedPage(driver).ClickOnContinueButton();
+//        new MainMenuPage(driver).ClickOnDeleteButton();
+//        new AccountDeletedPage(driver).AssertOnDeletionAnAccount("ACCOUNT DELETED!");
 
 
     }
